b13ddc85a2afe45aebb8791ffb9d7b7a385a5b91
ivkamluk <kamluk.jeck3ser@gmail.com>
2021-01-10 22:09:02 +0200

    Added support of PageFactory initialization

--- src/main/java/com/webpage/HomePage.java
+++ src/main/java/com/webpage/HomePage.java
@@ -1,19 +1,25 @@
 package com.webpage;
 
 import org.apache.log4j.Logger;
-import org.openqa.selenium.By;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class HomePage extends BasePage {
 
-       private final String GooglePageUrl = "https://google.com";
-       private WebElement buttonSearch;
-       private WebElement inputField;
+    private final String GooglePageUrl = "https://google.com";
+
+    @FindBy(xpath="//input[@name='btnK']")
+    private WebElement buttonSearch;
+
+    @FindBy(xpath="//input[@name='q']")
+    private WebElement inputField;
 
     public HomePage(WebDriver driver) {
         super(driver);
         log = Logger.getLogger(HomePage.class);
+        PageFactory.initElements(driver, this);
     }
 
     public void openHomePage() {
@@ -31,9 +37,7 @@ To test url from commandLine
   */
 
    public void performSearchByRequest(String searchRequest) {
-        inputField = driver.findElement(By.xpath("//input[@name='q']"));
         inputField.sendKeys(searchRequest);
-        buttonSearch = driver.findElement(By.xpath("//input[@name='btnK']"));
         waitElementLoadedToClick(buttonSearch);
         buttonSearch.click();
         log.info(searchRequest + " request was performed.");
--- src/main/java/com/webpage/SearchPage.java
+++ src/main/java/com/webpage/SearchPage.java
@@ -1,7 +1,8 @@
 package com.webpage;
 
 import org.apache.log4j.Logger;
-import org.openqa.selenium.By;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,42 +12,43 @@ import java.util.List;
 
 public class SearchPage extends BasePage {
 
-    private List<WebElement> linksFromSearch;
+    @FindBy(xpath="//*[@id='rso']/div/div/div/a")
+    private List<WebElement> searchedLinks;
+
+    @FindBy(xpath="//*[@id='rso']/div[1]/div/div/div/div[1]/a")
     private WebElement specificFirstBlockFromSearch;
+
+    private List<WebElement> linksOnSearchPage;
 
 
     public SearchPage(WebDriver driver) {
         super(driver);
-        linksFromSearch = new ArrayList<WebElement>();
+        linksOnSearchPage = new ArrayList<WebElement>();
         log = Logger.getLogger(SearchPage.class);
+        PageFactory.initElements(driver, this);
     }
 
     public void openLinkFromSearch(int linkIndex) throws InterruptedException {
         Thread.sleep(7000); // use Thread only for personal debug: to compare real page with opened index page
         waitElementLoadedToClick(getLinkFromSearchByIndex(linkIndex));
-        getLinkFromSearchByIndex(linkIndex).click();
+        linksOnSearchPage.get(linkIndex).click();
         log.info(driver.getCurrentUrl() + "link by requested index - " + linkIndex + ".");
-    }
-
-    private List<WebElement> getLinksFromSearch(String path) {
-
-        if (determineSpecificFirstBlockByRequest() != null) {
-            linksFromSearch.add(specificFirstBlockFromSearch);
-        }
-
-        linksFromSearch.addAll(driver.findElements(By.xpath(path)));
-        return linksFromSearch;
     }
 
     private WebElement getLinkFromSearchByIndex(int linkIndex) {
         // to open link according to the searched result we should decrease by 1
         // in list numeration always starts from 0 and 1st link will be on position 0
-        return getLinksFromSearch("//*[@id='rso']/div/div/div/a").get(linkIndex-1);
+
+        if (determineSpecificFirstBlockByRequest() != null) {
+           linksOnSearchPage.add(specificFirstBlockFromSearch);
+        }
+
+        linksOnSearchPage.addAll(searchedLinks);
+        return linksOnSearchPage.get(linkIndex-1);
     }
 
     private WebElement determineSpecificFirstBlockByRequest() {
         try {
-            specificFirstBlockFromSearch = driver.findElement(By.xpath("//*[@id='rso']/div[1]/div/div/div/div[1]/a"));
             return specificFirstBlockFromSearch;
         }
         catch(NoSuchElementException exception) {
